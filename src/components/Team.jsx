83c8c5991da44cd9e974c0815a41276573561bf4
Saurabh Ranjan <SaurabhR@proarch.com>
2024-03-08 20:54:24 +0530

    First code push

---
+++ src/components/Team.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import {
+  Container,
+  Typography,
+  Grid,
+  Card,
+  CardContent,
+  CardMedia,
+} from "@mui/material";
+
+export const Team = (props) => {
+  return (
+    <div id="team" style={{ textAlign: "center" }}>
+      <Container>
+        <Typography
+          variant="h1"
+          gutterBottom
+          style={{ fontWeight: "bold", color: "black" }}
+        >
+          Meet the Team
+        </Typography>
+        <Typography variant="h4" paragraph sx={{ marginBottom: "20px" }}>
+          We are led by the team who constantly questions, think, and challenges
+          to unlock great creativity around every turn.
+        </Typography>
+        <Grid container spacing={2} justifyContent="center">
+          {props.data
+            ? props.data.map((d, i) => (
+                <Grid key={`${d.name}-${i}`} item xs={12} sm={6} md={3}>
+                  <Card sx={{ flex: 1 }}>
+                    <CardMedia component="img" image={d.img} alt={d.name} />
+                    <CardContent>
+                      <Typography
+                        variant="h4"
+                        component="div"
+                        sx={{
+                          marginBottom: "8px",
+                        }}
+                      >
+                        {d.name}
+                      </Typography>
+                      <Typography
+                        variant="h4"
+                        sx={{ marginBottom: "4px", fontWeight: "bold" }}
+                      >
+                        {d.job}
+                      </Typography>
+                      <Typography variant="h5" color="text.secondary">
+                        {d.description}
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              ))
+            : "loading"}
+        </Grid>
+      </Container>
+    </div>
+  );
+};
---
+++ src/components/header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Box, AppBar } from "@mui/material";
+
+export const Header = (props) => {
+  return (
+    <AppBar position="static">
+      <Box className="intro">
+        <Box className="overlay">
+          <Box className="container">
+            <Box className="row">
+              <Box className="col-md-8 col-md-offset-2 intro-text">
+                <h1>
+                  {props.data ? props.data.title : "Loading"}
+                  <span></span>
+                </h1>
+                <p>{props.data ? props.data.paragraph : "Loading"}</p>
+                <a
+                  href="#features"
+                  className="btn btn-custom btn-lg page-scroll"
+                >
+                  Learn More
+                </a>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </AppBar>
+  );
+};
